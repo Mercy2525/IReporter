53065128ca694f633688bb4399801c60aa9424e5
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-27 23:33:51 +0300

    Feat: footer details

---
+++ client/src/components/FooterDetails.js
@@ -0,0 +1,34 @@
+export const FooterDetails=[
+{
+    title:'About',
+    point1: 'About I-Reporter',
+    point2: 'News',
+    point3: 'Get Involved',
+    point4: 'What is corruption?',
+    point5: 'Contact'
+},
+{
+    title:'Our Work',
+    point1: 'Social Justice',
+    point2: 'Discrimination',
+    point3: 'Asylum Seekers',
+    point4: 'Human Rights',
+    // point5: 'Contact'
+},
+{
+    title:'Complaints',
+    point1: 'About I-Reporter',
+    point2: 'News',
+    point3: 'Get Involved',
+    point4: 'What is corruption?',
+    point5: 'Contact'
+},
+{
+    title:'Complaints',
+    point1: 'Make a complaint',
+    point2: 'Complaint Information',
+    point3: 'How Corruption report works',
+    point4: 'Responding to a Complaint',
+    point5: 'Contact'
+}
+]
