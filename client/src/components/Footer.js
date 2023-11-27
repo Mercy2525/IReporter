4335eb09b43f0a227d217e494d6560a3c540b7b8
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-27 23:54:43 +0300

    feat:Updated footer details

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,44 @@
 import React from 'react'
-import { FooterDetails } from './FooterDetails'
+import { BsFacebook } from "react-icons/bs";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsTwitter } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 function Footer() {
+    const FooterDetails=[
+        {
+            title:'About',
+            point1: 'About I-Reporter',
+            point2: 'News',
+            point3: 'Get Involved',
+            point4: 'What is corruption?',
+            point5: 'Contact'
+        },
+        {
+            title:'Our Work',
+            point1: 'Social Justice',
+            point2: 'Discrimination',
+            point3: 'Asylum Seekers',
+            point4: 'Human Rights',
+            // point5: 'Contact'
+        },
+        {
+            title:'Complaints',
+            point1: 'Make a complaint',
+            point2: 'Complaint Information',
+            point3: 'How Corruption report works',
+            point4: 'Responding to a Complaint',
+            point5: 'Contact'
+        },
+        {
+            title:'Socials',
+            point1: <BsFacebook className="text-4xl"/> ,
+            point2: <RiInstagramFill className="text-4xl"/>,
+            point3: <BsTwitter className="text-4xl"/>,
+            point4: <MdEmail className="text-4xl"/>
+        }
+        ]
+        
   return (
     <div>
         <div className='bg-color-gray mt-2 rounded-lg'>
@@ -13,7 +50,7 @@ function Footer() {
                         <p className='rounded-lg overflow-hidden ml-4 border border-color-tertiary p-2'> {footer.title} </p>
                     </div>
 
-                    <div className="px-6 text-xl py-4 text-white leading-normal ">
+                    <div className="px-6 text-xl py-4 text-white leading-8 ">
                         <p> {footer.point1} </p>
                         <p> {footer.point2} </p>
                         <p> {footer.point3} </p>
