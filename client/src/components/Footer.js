b49a1e1944a058eb19999396c1aaad86db14689e
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-28 01:19:57 +0300

    more changes to the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,8 +11,7 @@ function Footer() {
             point1: 'About I-Reporter',
             point2: 'News',
             point3: 'Get Involved',
-            point4: 'What is corruption?',
-            point5: 'Contact'
+            point4: 'What is corruption?'
         },
         {
             title:'Our Work',
@@ -26,44 +25,46 @@ function Footer() {
             title:'Complaints',
             point1: 'Make a complaint',
             point2: 'Complaint Information',
-            point3: 'How Corruption report works',
-            point4: 'Responding to a Complaint',
-            point5: 'Contact'
+            point3: 'How I-report works',
+            point4: 'Response to Complaints'
         },
         {
             title:'Socials',
-            point1: <BsFacebook className="text-4xl"/> ,
-            point2: <RiInstagramFill className="text-4xl"/>,
-            point3: <BsTwitter className="text-4xl"/>,
-            point4: <MdEmail className="text-4xl"/>
+            point1: <p className='flex'> <BsFacebook className='text-3xl p-1'/> Facebook </p> ,
+            point2: <p className='flex'> <RiInstagramFill className='text-3xl p-1'/> Instagram </p>,
+            point3: <p className='flex'> <BsTwitter className='text-3xl p-1'/> Twitter </p>,
+            point4: <p className='flex'> <MdEmail className='text-3xl p-1'/> Email Us</p>
         }
         ]
-        
+
   return (
-    <div>
-        <div className='bg-color-gray mt-2 rounded-lg'>
-        <div className=' m-auto justify-evenly flex flex-row flex-wrap '>  
-            {FooterDetails.map((footer,idx)=>(
-                <div key={idx} className=" max-w-sm my-8 overflow-hidden  p-2">
+    
+    <footer className="bg-color-gray py-4 w-full sm:fixed sm:bottom-0  ">
 
-                    <div className='text-2xl flex  items-center '>
-                        <p className='rounded-lg overflow-hidden ml-4 border border-color-tertiary p-2'> {footer.title} </p>
-                    </div>
+        <div className='mt-2 rounded-lg'>
 
-                    <div className="px-6 text-xl py-4 text-white leading-8 ">
-                        <p> {footer.point1} </p>
-                        <p> {footer.point2} </p>
-                        <p> {footer.point3} </p>
-                        <p> {footer.point4} </p>
-                        <p> {footer.point5} </p>
-                    </div>   
+            <div className=' m-auto justify-evenly flex flex-row flex-wrap  '>  
+                {FooterDetails.map((footer,idx)=>(
+                    <div key={idx} className=" max-w-sm my-8 overflow-hidden p-2  ">
 
-                </div> 
-            ))}
+                        <div className='text-2xl flex  items-center '>
+                            <p className='rounded-lg overflow-hidden ml-4 border border-color-tertiary p-2'> {footer.title} </p>
+                        </div>
+
+                        <div className="px-6 text-xl py-4 text-white leading-8 ">
+                            <p> {footer.point1} </p>
+                            <p> {footer.point2} </p>
+                            <p> {footer.point3} </p>
+                            <p> {footer.point4} </p>
+                        </div>   
+
+                    </div> 
+                ))}
+            </div>
+
         </div>
-
-        </div>
-    </div>
+    </footer>
+    
   )
 }
 
