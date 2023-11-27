766eff28fb931cca7dedbc39270c0fb2d562f33e
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-28 01:20:58 +0300

    feat:images

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,10 +5,10 @@ import no_corruption from '../assets/no_corruption.webp'
 function Home() {
   return (
     <div>
-        <div className='bg-color-secondary max-h-full rounded-lg flex flex-row flex-wrap'>
+        <div className='bg-color-secondary max-h-full rounded-lg flex flex-row flex-wrap sm:flex-col sm:flex-wrap'>
 
-        <img className='rounded-lg p-3 ml-10' src={corruption}/>
-        <img className='rounded-lg p-3 ml-10' src={no_corruption}/>
+        <img className='rounded-lg p-3 ml-10' src={corruption} alt='corruption'/>
+        <img className='rounded-lg p-3 ml-10' src={no_corruption} alt='no_corruption'/>
     
         
         </div>
