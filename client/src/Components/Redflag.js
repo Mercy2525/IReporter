b8005bbd867dbfd4adbb4ef99c3f9bbc28779e71
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-29 07:57:17 +0300

    changed-folder-structure

--- client/src/Components/Redflag.js
+++ client/src/Components/Redflag.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Redflag.css';
+import '../styles/Redflag.css'
 
 const RedFlag = () => {
   const [redFlags, setRedFlags] = useState([]);
