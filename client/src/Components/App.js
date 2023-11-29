0b75e7f3aed51ad4a75e6a8919eeeb543d3a0e40
brianbryak <brianbryan0125@gmail.com>
2023-11-29 20:59:42 +0300

    login and signup files

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,18 +2,36 @@ import {Route, Routes} from 'react-router-dom';
 import Redflag from './Redflag.js';
 import Navbar from "../pages/Navbar.js";
 import Home from '../pages/Home.js';
+import SignUp from './SignUp';
+import Login from './Login';
+import Landing from './Landing';
+import { useState, useEffect } from 'react';
 
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch('/session')
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((error) => console.error('Error fetching session:', error));
+  }, []);
+
+
   return (
     <div className="App">
 
       <Routes>
 
-        <Route element={<Navbar/>}>
+        <Route element={<Navbar user={user}/>}>
           <Route element={<Home/>} path="/"/>
           <Route element={<Home/>} path="/home"/>
           <Route path="/redflag" element={<Redflag />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
 
         </Route>
       </Routes>
---
+++ client/src/Components/Login.js
@@ -0,0 +1,88 @@
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import YupPassword from 'yup-password';
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+function Login({ updateUser }) {
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+
+  function handleSignUpClick() {
+    navigate('/signup');
+  }
+
+  YupPassword(Yup);
+  const errorMessagesSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('This field is required'),
+    password: Yup.string()
+      .required('This field is required')
+      .min(6, 'Password must be at least 6 characters'),
+  });
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center p-20'>
+        <h1 className='font-bold text-2xl mb-8'> Log In</h1>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={errorMessagesSchema}
+          onSubmit={(values, { resetForm }) => {
+            fetch('https://ireporter-backend.onrender.com/login_user', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(values),
+            })
+              .then((res) => {
+                if (res.status === 200) {
+                  enqueueSnackbar('Log in successful!', { variant: 'success' });
+                  navigate('/home');
+                } else {
+                  return res.json().then((data) => {
+                    enqueueSnackbar(data.message || 'Invalid username or password', { variant: 'error' });
+                    console.log(data); // Log the response for debugging
+                  });
+                }
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+                enqueueSnackbar('Something went wrong', { variant: 'error' });
+              })
+              .finally(() => {
+                resetForm();
+              });
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form className='flex flex-col content-center justify-center max-w-xs w-full'>
+              <label className='m-2 font-bold' htmlFor='email'>
+                Email address
+              </label>
+              <Field type='text' name='email' id='email' className='text-rich-black px-2 rounded' />
+              {touched.email && errors.email && <div className='text-indian-red'>{errors.email}</div>}
+
+              <label className='m-2 font-bold' htmlFor='password'>
+                Password
+              </label>
+              <Field type='password' name='password' id='password' className='text-rich-black px-2 rounded' />
+              {touched.password && errors.password && <div className='text-indian-red'>{errors.password}</div>}
+
+              <Button type='submit' content='Log In' className='text-sm my-5 mx-auto py-2  w-2/6' />
+            </Form>
+          )}
+        </Formik>
+        <p>
+          Don't have an account? <span className='font-bold' onClick={handleSignUpClick}>Sign Up</span>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Login;
