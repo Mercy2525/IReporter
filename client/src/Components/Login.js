de261e435b5732e534f132cf4fb6b45b6fb2f446
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-30 10:44:29 +0300

    feat:user login done

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -4,6 +4,7 @@ import YupPassword from 'yup-password';
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
+import {Link} from 'react-router-dom'
 
 function Login({ updateUser }) {
   const { enqueueSnackbar } = useSnackbar();
@@ -15,7 +16,9 @@ function Login({ updateUser }) {
 
   YupPassword(Yup);
   const errorMessagesSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('This field is required'),
+    email: Yup.string()
+    .email('Invalid email').
+    required('This field is required'),
     password: Yup.string()
       .required('This field is required')
       .min(6, 'Password must be at least 6 characters'),
@@ -24,28 +27,28 @@ function Login({ updateUser }) {
   return (
     <>
       <div className='flex flex-col items-center justify-center p-20'>
-        <h1 className='font-bold text-2xl mb-8'> Log In</h1>
+        <h1 className='font-semiBold text-color-blue2 text-2xl mb-8'>User Log In</h1>
         <Formik
           initialValues={{
             email: '',
             password: '',
           }}
           validationSchema={errorMessagesSchema}
-          onSubmit={(values, { resetForm }) => {
+          onSubmit={(values, e) => {
             fetch('https://ireporter-backend.onrender.com/login_user', {
-              method: 'POST',
+              method: "POST",
               headers: {
                 'Content-Type': 'application/json',
               },
               body: JSON.stringify(values),
             })
               .then((res) => {
-                if (res.status === 200) {
+                if (res.status === 201) {
                   enqueueSnackbar('Log in successful!', { variant: 'success' });
                   navigate('/home');
                 } else {
                   return res.json().then((data) => {
-                    enqueueSnackbar(data.message || 'Invalid username or password', { variant: 'error' });
+                    enqueueSnackbar(data.message || 'Invalid email or password', { variant: 'error' });
                     console.log(data); // Log the response for debugging
                   });
                 }
@@ -55,31 +58,46 @@ function Login({ updateUser }) {
                 enqueueSnackbar('Something went wrong', { variant: 'error' });
               })
               .finally(() => {
-                resetForm();
+                e.resetForm();
               });
           }}
         >
           {({ errors, touched }) => (
-            <Form className='flex flex-col content-center justify-center max-w-xs w-full'>
+            <Form className='flex flex-col content-center mb-1 justify-center bg-color-blue   max-w-xs w-full'>
               <label className='m-2 font-bold' htmlFor='email'>
                 Email address
               </label>
-              <Field type='text' name='email' id='email' className='text-rich-black px-2 rounded' />
-              {touched.email && errors.email && <div className='text-indian-red'>{errors.email}</div>}
+              <Field type='email' name='email' id='email' className='text-rich-black px-2 rounded' />
+              {touched.email && errors.email && <div className='text-color-red'>{errors.email}</div>}
 
               <label className='m-2 font-bold' htmlFor='password'>
                 Password
               </label>
               <Field type='password' name='password' id='password' className='text-rich-black px-2 rounded' />
-              {touched.password && errors.password && <div className='text-indian-red'>{errors.password}</div>}
+              {touched.password && errors.password && <div className='text-color-red'>{errors.password}</div>}
 
-              <Button type='submit' content='Log In' className='text-sm my-5 mx-auto py-2  w-2/6' />
+              <Button type='submit' content='Log In' className='text-sm bg-color-blue2 my-5 mx-auto py-2  w-2/6' />
             </Form>
           )}
         </Formik>
         <p>
-          Don't have an account? <span className='font-bold' onClick={handleSignUpClick}>Sign Up</span>
+          Don't have an account? <span className='font-bold ' onClick={handleSignUpClick}>Sign Up</span>
         </p>
+
+        <h1 className='mt-6 font-bold'>
+            <span className='inline-flex items-center border-t-2  border-color-blue2'>
+                <span className='text-color-secondary '>OR</span>
+            </span>
+        </h1>
+
+        <Link to='/adminLogin'>
+            <Button type='button' content='Log In as Admin' className='text-sm shadow-lg  bg-color-blue2 my-5 mx-auto py-2  w-80' />
+        </Link>
+
+       
+
+
+       
       </div>
     </>
   );
