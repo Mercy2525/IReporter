da96cd2370bf82861667b6bc21dfb05d4834ab43
Mercy <mercywmuriithi.mm@gmail.com>
2023-12-05 22:31:57 +0300

    feat: persisted session

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -10,9 +10,10 @@ import AdminLogin from './AdminLogin.js';
 import Users from '../Dashboard/Users.js';
 import Redflags from '../Dashboard/Redflag.js';
 import Interventions from '../Dashboard/Intervention.js';
-
+import UserIntervention from './UserIntervention.js'
 import NotFound from '../pages/NotFound.js'
 import Landing from './Landing.js';
+
 
 
 
@@ -20,26 +21,29 @@ function App() {
 
   const [user, setUser] = useState({});
   const [admin, setAdmin] = useState({});
-  // const[isLoggedIn, setIsLoggedIn]=useState(false)
+  const[refresh, setRefresh]=useState(false)
+
  
   //user-session
   useEffect(() => {
-    fetch("https://ireporter-backend.onrender.com/session_user")
+    fetch("/session_user")
     .then(response=>{
       if (response.ok){
-        response.json()
+        return response.json()
       }
     })
     .then(data=>setUser(data))
     .catch(error => console.log(error));
-  }, []); 
+  }, [refresh]); 
+
+  // console.log(redflags)
 
   //admin session
   useEffect(() => {
-    fetch("https://ireporter-backend.onrender.com/session_admin")
+    fetch("/session_admin")
     .then(response=>{
       if (response.ok){
-        response.json()
+        return response.json()
       }
     })
     .then(data=>setAdmin(data))
@@ -60,7 +64,8 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login setUser={setUser}  />} />
           <Route path="/adminLogin" element={<AdminLogin setAdmin={setAdmin}/>}/>
-          <Route path='/landing' element={<Landing user={user}/>}/>
+          <Route path='/landing' element={<Landing user={user} refresh={refresh} setRefresh={setRefresh}/>}/>
+          <Route path='/user/intervention' element={<UserIntervention user={user} refresh={refresh} setRefresh={setRefresh}/>}/>
       </Route>
 
           <Route path='*' element={<NotFound/>}/>
@@ -68,6 +73,7 @@ function App() {
           <Route path="/usersdashboard" element={<Users/>}/>
           <Route path="/redflagsdashboard" element={<Redflags/>}/>
           <Route path="/interventionsdashboard" element={<Interventions/>}/>
+         
       </Routes>
 
 
